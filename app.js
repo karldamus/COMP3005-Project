3269c583613057f4bf36ab8b45afbdf6e688f9b3
Karl Damus <getaholdofkarl@gmail.com>
2022-10-31 01:04:15 -0400

    Moved old test data to routes

--- app.js
+++ app.js
@@ -1,77 +1,29 @@
-// IF YOU'RE DEPLOYING
-// CHANGE TO FALSE
-// 
-const devMode = true;
-// ====================
+let CONSTANTS = require('./public/constants/basic');
 
 // create a new express app
 const express = require('express');
 const app = express();
 const mysql = require('mysql2');
 
-const secrets = require('./secrets.json');
-
-// database connection variable
-var con;
+// data for the database
+const Books = require('./public/data/Books.json');
 
 // add & configure middleware
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Testing node server third time');
+    res.send("Hello world");
 });
 
+// use books router under /books
+app.use('/books', require('./routes/BookDataRouter'));
 
-app.get('/db', (req, res) => {
-    res.send(connectToDatabase());
-});
+// use db router under /db
+app.use('/db', require('./routes/DatabaseRouter'));
 
-app.get('/db/get', (req, res) => {
-    connectToDatabase();
-    // let data = getDataFromDatabase();
-    // res.send(data);
-    con.query('SELECT * FROM `test`', function(err, rows, fields) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else {
-            console.log(rows);
-            res.send(rows);
-        }
-    });
-});
-
-function connectToDatabase() {
-    if (devMode) {
-        con = mysql.createConnection(secrets.dev_mode_config);
-    } else {
-        con = mysql.createConnection(secrets.prod_mode_config);
-    }
-
-    con.connect(function(err) {
-        if (err) {
-            console.log('Error connecting to Db');
-        }
-
-        console.log('Connection established');
-    });
-
-    return "Connection established";
-}
-
-function disconnectFromDatabase() {
-    con.end(function(err) {
-        if (err) {
-            console.log('Error disconnecting from Db');
-            return;
-        }
-
-        console.log('Connection closed');
-        return;
-    });
-}
-
-if (devMode) {
+// 
+// START APP
+if (CONSTANTS.DEV_MODE) {
     // start app -- local version
     app.listen(3000, () => {
         console.log('listening on 3000');
