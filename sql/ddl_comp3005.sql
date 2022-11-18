a7205aa2718000e8a315ec2467fa8084ccf66824
Karl Damus <getaholdofkarl@gmail.com>
2022-11-18 00:55:54 -0500

    Update colum names

--- sql/ddl_comp3005.sql
+++ sql/ddl_comp3005.sql
@@ -1,98 +1,98 @@
 CREATE TABLE IF NOT EXISTS PUBLISHERS (
-    publisher_db_id                  INT PRIMARY KEY AUTO_INCREMENT,
+    db_id                  INT PRIMARY KEY AUTO_INCREMENT,
     publisher_id        VARCHAR(6) UNIQUE NOT NULL,
-    publisher_house     VARCHAR(30) NOT NULL,
-    publisher_city                VARCHAR(30),
-    publisher_state               VARCHAR(30),
-    publisher_country             VARCHAR(30),
-    publisher_year_established    INT
+    house     VARCHAR(30) NOT NULL,
+    city                VARCHAR(30),
+    state               VARCHAR(30),
+    country             VARCHAR(30),
+    year_established    INT
 );
 
 CREATE TABLE IF NOT EXISTS AUTHORS (
-    author_db_id          INT PRIMARY KEY AUTO_INCREMENT,
+    db_id          INT PRIMARY KEY AUTO_INCREMENT,
     author_id   VARCHAR(6) UNIQUE NOT NULL,
-    author_name        VARCHAR(20) NOT NULL
+    name        VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS BOOKS (
-    book_db_id              INT PRIMARY KEY AUTO_INCREMENT,
+    db_id              INT PRIMARY KEY AUTO_INCREMENT,
     book_id         VARCHAR(6) UNIQUE NOT NULL,
-    book_name            VARCHAR(100) NOT NULL,
-    book_ISBN            VARCHAR(20) NOT NULL,
-    book_genre           VARCHAR(20) NOT NULL,
-    book_price           FLOAT(4) NOT NULL,
-    book_num_pages       INT NOT NULL,
-    book_publisher_id    VARCHAR(6) NOT NULL,
-    book_author_id       VARCHAR(6) NOT NULL,
+    name            VARCHAR(100) NOT NULL,
+    ISBN            VARCHAR(20) NOT NULL,
+    genre           VARCHAR(20) NOT NULL,
+    price           FLOAT(4) NOT NULL,
+    num_pages       INT NOT NULL,
+    publisher_id    VARCHAR(6) NOT NULL,
+    author_id       VARCHAR(6) NOT NULL,
 
-    FOREIGN KEY (book_publisher_id)
+    FOREIGN KEY (publisher_id)
         REFERENCES PUBLISHERS (publisher_id),
-    FOREIGN KEY (book_author_id)
+    FOREIGN KEY (author_id)
         REFERENCES AUTHORS (author_id)
 );
 
 CREATE TABLE IF NOT EXISTS SHIPPING_ADDRESS (
-    shipping_address_db_id          INT PRIMARY KEY AUTO_INCREMENT,
-    shipping_address_street_1    VARCHAR(30) NOT NULL,
-    shipping_address_street_2    VARCHAR(30),
-    shipping_address_country     VARCHAR(30) NOT NULL,
-    shipping_address_city        VARCHAR(30) NOT NULL,
-    shipping_address_province    VARCHAR(30) NOT NULL,
-    shipping_address_postal_code VARCHAR(6) NOT NULL 
+    db_id          INT PRIMARY KEY AUTO_INCREMENT,
+    street_1    VARCHAR(30) NOT NULL,
+    street_2    VARCHAR(30),
+    country     VARCHAR(30) NOT NULL,
+    city        VARCHAR(30) NOT NULL,
+    province    VARCHAR(30) NOT NULL,
+    postal_code VARCHAR(6) NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS BILLING_ADDRESS (
-    billing_address_db_id          INT PRIMARY KEY AUTO_INCREMENT,
-    billing_address_street_1    VARCHAR(30) NOT NULL,
-    billing_address_street_2    VARCHAR(30),
-    billing_address_country     VARCHAR(30) NOT NULL,
-    billing_address_city        VARCHAR(30) NOT NULL,
-    billing_address_province    VARCHAR(30) NOT NULL,
-    billing_address_postal_code VARCHAR(6) NOT NULL 
+    db_id          INT PRIMARY KEY AUTO_INCREMENT,
+    street_1    VARCHAR(30) NOT NULL,
+    street_2    VARCHAR(30),
+    country     VARCHAR(30) NOT NULL,
+    city        VARCHAR(30) NOT NULL,
+    province    VARCHAR(30) NOT NULL,
+    postal_code VARCHAR(6) NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS CREDIT_CARD (
-    credit_card_db_id   INT PRIMARY KEY AUTO_INCREMENT,
-    credit_card_name                VARCHAR(50),
-    credit_card_number              INT,  
-    credit_card_expiry_date         DATE,
-    credit_card_billing_address_id  INT,
+    db_id   INT PRIMARY KEY AUTO_INCREMENT,
+    name                VARCHAR(50),
+    number              INT,  
+    expiry_date         DATE,
+    billing_address_id  INT,
     
-    FOREIGN KEY (credit_card_billing_address_id)
-        REFERENCES BILLING_ADDRESS (billing_address_db_id)
+    FOREIGN KEY (billing_address_id)
+        REFERENCES BILLING_ADDRESS (db_id)
 );
 
 CREATE TABLE IF NOT EXISTS USERS (
-    user_db_id              INT PRIMARY KEY AUTO_INCREMENT,
-    user_level                   INT NOT NULL,
-    user_name                    VARCHAR(50) NOT NULL,
-    user_username                VARCHAR(50) UNIQUE NOT NULL,
-    user_password                VARCHAR(150) NOT NULL,
-    user_shipping_address_id     INT,
-    user_credit_card_id          INT,
+    db_id              INT PRIMARY KEY AUTO_INCREMENT,
+    level                   INT NOT NULL,
+    name                    VARCHAR(50) NOT NULL,
+    username                VARCHAR(50) UNIQUE NOT NULL,
+    password                VARCHAR(150) NOT NULL,
+    shipping_address_id     INT,
+    credit_card_id          INT,
 
-    FOREIGN KEY (user_shipping_address_id)
-        REFERENCES SHIPPING_ADDRESS (shipping_address_db_id),
-    FOREIGN KEY (user_credit_card_id)
-        REFERENCES CREDIT_CARD (credit_card_db_id)
+    FOREIGN KEY (shipping_address_id)
+        REFERENCES SHIPPING_ADDRESS (db_id),
+    FOREIGN KEY (credit_card_id)
+        REFERENCES CREDIT_CARD (db_id)
 );
 
 CREATE TABLE IF NOT EXISTS ORDERS (
-    order_db_id      INT PRIMARY KEY AUTO_INCREMENT,
-    order_status  INT NOT NULL,
-    order_users_id INT,
+    db_id      INT PRIMARY KEY AUTO_INCREMENT,
+    status  INT NOT NULL,
+    user_id INT,
 
-    FOREIGN KEY(order_users_id)
-        REFERENCES USERS(user_db_id)
+    FOREIGN KEY(user_id)
+        REFERENCES USERS(db_id)
 );  
 
 CREATE TABLE IF NOT EXISTS ORDER_ITEMS (
-    order_item_quantity    INT NOT NULL,
-    order_item_order_id    INT NOT NULL,
-    order_item_book_id     VARCHAR(6) NOT NULL,
+    quantity    INT NOT NULL,
+    order_id    INT NOT NULL,
+    book_id     VARCHAR(6) NOT NULL,
     
-    FOREIGN KEY (order_item_order_id)
-        REFERENCES ORDERS (order_db_id),
-    FOREIGN KEY (order_item_book_id)
+    FOREIGN KEY (order_id)
+        REFERENCES ORDERS (db_id),
+    FOREIGN KEY (book_id)
         REFERENCES BOOKS (book_id)
 );
