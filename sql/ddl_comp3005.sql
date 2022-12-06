a80652bb4845e921ac558cd54635a0daadde0c7c
Karl Damus <getaholdofkarl@gmail.com>
2022-12-06 07:41:52 -0500

    Update DDL statements

--- sql/ddl_comp3005.sql
+++ sql/ddl_comp3005.sql
@@ -1,22 +1,39 @@
+CREATE TABLE IF NOT EXISTS BANK_ACCOUNTS (
+    account_number      INT UNIQUE NOT NULL,
+    balance             DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS TRANSACTIONS (
+    id                  INT AUTO_INCREMENT PRIMARY KEY,
+    account_number      INT NOT NULL,
+    transaction_date    DATE NOT NULL,
+    transaction_amount  DECIMAL(10,2) NOT NULL,
+    to_account_number   INT NOT NULL,
+
+    FOREIGN KEY (account_number) 
+        REFERENCES BANK_ACCOUNTS(account_number)
+);
+
 CREATE TABLE IF NOT EXISTS PUBLISHERS (
-    db_id                  INT PRIMARY KEY AUTO_INCREMENT,
-    publisher_id        VARCHAR(6) UNIQUE NOT NULL,
-    house     VARCHAR(30) NOT NULL,
+    id                  VARCHAR(6) UNIQUE NOT NULL,
+    house               VARCHAR(30) NOT NULL,
     city                VARCHAR(30),
     state               VARCHAR(30),
     country             VARCHAR(30),
-    year_established    INT
+    year_established    INT,
+    bank_account_number INT,
+
+    FOREIGN KEY (bank_account_number) 
+        REFERENCES BANK_ACCOUNTS(account_number)
 );
 
 CREATE TABLE IF NOT EXISTS AUTHORS (
-    db_id          INT PRIMARY KEY AUTO_INCREMENT,
-    author_id   VARCHAR(6) UNIQUE NOT NULL,
+    id          VARCHAR(6) UNIQUE NOT NULL,
     name        VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS BOOKS (
-    db_id              INT PRIMARY KEY AUTO_INCREMENT,
-    book_id         VARCHAR(6) UNIQUE NOT NULL,
+    id              VARCHAR(6) UNIQUE NOT NULL,
     name            VARCHAR(100) NOT NULL,
     ISBN            VARCHAR(20) NOT NULL,
     genre           VARCHAR(20) NOT NULL,
@@ -26,15 +43,14 @@ CREATE TABLE IF NOT EXISTS BOOKS (
     author_id       VARCHAR(6) NOT NULL,
 
     FOREIGN KEY (publisher_id)
-        REFERENCES PUBLISHERS (publisher_id),
+        REFERENCES PUBLISHERS (id),
     FOREIGN KEY (author_id)
-        REFERENCES AUTHORS (author_id)
+        REFERENCES AUTHORS (id)
 );
 
 CREATE TABLE IF NOT EXISTS SHIPPING_ADDRESS (
-    db_id          INT PRIMARY KEY AUTO_INCREMENT,
-    street_1    VARCHAR(30) NOT NULL,
-    street_2    VARCHAR(30),
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    street    VARCHAR(30) NOT NULL,
     country     VARCHAR(30) NOT NULL,
     city        VARCHAR(30) NOT NULL,
     province    VARCHAR(30) NOT NULL,
@@ -42,9 +58,8 @@ CREATE TABLE IF NOT EXISTS SHIPPING_ADDRESS (
 );
 
 CREATE TABLE IF NOT EXISTS BILLING_ADDRESS (
-    db_id          INT PRIMARY KEY AUTO_INCREMENT,
-    street_1    VARCHAR(30) NOT NULL,
-    street_2    VARCHAR(30),
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    street    VARCHAR(30) NOT NULL,
     country     VARCHAR(30) NOT NULL,
     city        VARCHAR(30) NOT NULL,
     province    VARCHAR(30) NOT NULL,
@@ -52,38 +67,40 @@ CREATE TABLE IF NOT EXISTS BILLING_ADDRESS (
 );
 
 CREATE TABLE IF NOT EXISTS CREDIT_CARD (
-    db_id   INT PRIMARY KEY AUTO_INCREMENT,
+    id                  INT AUTO_INCREMENT PRIMARY KEY,
     name                VARCHAR(50),
     number              INT,  
     expiry_date         DATE,
     billing_address_id  INT,
     
     FOREIGN KEY (billing_address_id)
-        REFERENCES BILLING_ADDRESS (db_id)
+        REFERENCES BILLING_ADDRESS (id)
 );
 
 CREATE TABLE IF NOT EXISTS USERS (
-    db_id              INT PRIMARY KEY AUTO_INCREMENT,
+    id                      INT AUTO_INCREMENT PRIMARY KEY,
     level                   INT NOT NULL,
     name                    VARCHAR(50) NOT NULL,
     username                VARCHAR(50) UNIQUE NOT NULL,
     password                VARCHAR(150) NOT NULL,
+    save_info               BIT,
     shipping_address_id     INT,
     credit_card_id          INT,
 
+
     FOREIGN KEY (shipping_address_id)
-        REFERENCES SHIPPING_ADDRESS (db_id),
+        REFERENCES SHIPPING_ADDRESS (id),
     FOREIGN KEY (credit_card_id)
-        REFERENCES CREDIT_CARD (db_id)
+        REFERENCES CREDIT_CARD (id)
 );
 
 CREATE TABLE IF NOT EXISTS ORDERS (
-    db_id      INT PRIMARY KEY AUTO_INCREMENT,
+    id      INT AUTO_INCREMENT PRIMARY KEY,
     status  INT NOT NULL,
     user_id INT,
 
     FOREIGN KEY(user_id)
-        REFERENCES USERS(db_id)
+        REFERENCES USERS(id)
 );  
 
 CREATE TABLE IF NOT EXISTS ORDER_ITEMS (
@@ -92,7 +109,7 @@ CREATE TABLE IF NOT EXISTS ORDER_ITEMS (
     book_id     VARCHAR(6) NOT NULL,
     
     FOREIGN KEY (order_id)
-        REFERENCES ORDERS (db_id),
+        REFERENCES ORDERS (id),
     FOREIGN KEY (book_id)
-        REFERENCES BOOKS (book_id)
+        REFERENCES BOOKS (id)
 );
