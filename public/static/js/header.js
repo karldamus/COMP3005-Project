06ab68e8a0fff1b3f0d500c59b5b41f69d05d4e7
Karl Damus <getaholdofkarl@gmail.com>
2022-12-11 18:24:34 -0500

    add isLoggedIn check

--- public/static/js/header.js
+++ public/static/js/header.js
@@ -14,49 +14,34 @@ xhttp.send();
 
 
 function createHeader(htmlContent) {
-    // get the <my-header> element
-    let header = document.getElementsByTagName("my-header")[0];
-
-    // add the html content to the header div
-    header.innerHTML = htmlContent;
-
-    // get header-logo
-    let headerLogo = document.getElementById("header-logo");
-
-    // add onclick event to header-logo
-    headerLogo.onclick = function() {
-        location.href = "/";
-    };
-
     // check if user is logged in
     xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log("Check if user is logged in: " + this.responseText);
+            let isLoggedIn = this.responseText == "true" ? true : false;
 
-            // if (this.responseText) {
-            //     // enable #login-link
-            //     document.getElementById("login-link").style.display = "block";
+            // get the <my-header> element
+            let header = document.getElementsByTagName("my-header")[0];
 
-            //     // disable #logout-link
-            //     document.getElementById("logout-link").style.display = "none";
-            // } else {
-            //     // disable #login-link
-            //     document.getElementById("login-link").style.display = "none";
+            // add the html content to the header div
+            header.innerHTML = htmlContent;
 
-            //     // enable #logout-link
-            //     document.getElementById("logout-link").style.display = "block";
-            // }
+            // get header-logo
+            let headerLogo = document.getElementById("header-logo");
 
-            
-            // if (this.responseText) {
-            //     document.getElementById("login-link").innerHTML = "Logout";
-            //     document.getElementById("login-link").href = "/user/logout";
-            // } else {
-            //     document.getElementById("login-link").innerHTML = "Login";
-            //     document.getElementById("login-link").href = "/user/login";
-            // }
+            // add onclick event to header-logo
+            headerLogo.onclick = function() {
+                location.href = "/";
+            };
+
+            if (isLoggedIn) {
+                document.getElementById("login-text").innerHTML = "Logout";
+                document.getElementById("login-link").href = "/user/logout";
+            } else {
+                document.getElementById("login-text").innerHTML = "Login";
+                document.getElementById("login-link").href = "/user/login";
+            }
         }
     }
 
